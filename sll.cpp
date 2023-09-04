6b65fde2a15c4c0c892fc91cbf087aee01420136
Jaivanth B <beerakajaivanth15@gmail.com>
2023-09-04 20:44:50 +0530

    Update sll.cpp

--- sll.cpp
+++ sll.cpp
@@ -1,194 +1,177 @@
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
-
 struct node
 { int data;
   node* link;};
-
 struct header
 { int count;
-  node* front;
-  node* rear;};
-
+  node* first;};
 class SLL
-{ header *queue;
-  node *pred,*pnew,*temp2,*temp;
+{ header *head;
+  node *pnew,*ploc,*pred,*temp;
   public:
-  
   SLL()
-  { queue=new header;
-    queue->count=0;
-    queue->front=NULL;
-    queue->rear=NULL;}
-  
-  void enqueue()
+  { head=new header;
+    head->count=0;
+    head->first=NULL;}
+  void insert()
   { pnew = new node;
-    if(queue->count==0)
+    if(head->count==0)
     { cout<<"\nEnter data: ";
       cin>>pnew->data;
-      queue->front=pnew;
-      queue->rear=pnew;
+      head->first=pnew;
       temp=pnew;
-      queue->count++;
+      head->count++;
       cout<<"\nElement inserted";
       pnew->link=NULL;}
-    else if(queue->count>0)
+    else if(head->count>0)
     { cout<<"\nEnter data: ";
       cin>>pnew->data;
       temp->link=pnew;
       temp=pnew;
-      queue->rear=pnew;
-      queue->count++;
+      head->count++;
       cout<<"\nElement inserted";
       pnew->link=NULL;}
-    else if(pnew==NULL)
+    else if(&pnew==NULL)
     { cout<<"\nNot possible, Memory full";}}
-  
   void display()
-  { temp=queue->front;
-    if(queue->count==0)
+  { temp=head->first;
+    if(temp->link==NULL)
     {cout<<"\nList is empty";}
     else{
     int l;
     cout<<"\nWith address(1 for yes and 0 for no)?: ";
     cin>>l;
     if(l==1)
-    {while(temp->link!=NULL)
-     { cout<<"\nData= "<<temp->data;
-       cout<<"\nAddress= "<<temp->link;
-       temp=temp->link;}
-     cout<<"\nData= "<<temp->data;
-     cout<<"\nAddress= "<<temp->link;
-     cout<<"\n\nqueue Data";
-     cout<<"\nCount= "<<queue->count;
-     cout<<"\nLink= "<<queue->front;}
+    { while(temp->link!=NULL)
+      { cout<<"\nData= "<<temp->data;
+        cout<<"\nAddress= "<<temp->link;
+        temp=temp->link;}
+      cout<<"\nData= "<<temp->data;
+      cout<<"\nAddress= "<<temp->link;
+      cout<<"\n\nHeader Data";
+      cout<<"\nCount= "<<head->count;
+      cout<<"\nLink= "<<head->first;}
     else
-    { if(queue->count>1)
-      { cout<<"\nQueue SLL: ";
-        while(temp->link!=NULL)
-        { cout<<temp->data;
-          cout<<"->";
-          temp=temp->link;}
-        cout<<temp->data;}
-      else
-      { cout<<"\nQueue SLL: ";
-        cout<<temp->data;}}}}
-  
-  void dequeue()
-  { if(queue->count==0)
-    { cout<<"\nList is empty";}
-    else if(queue->count==2)
-    { temp=new node;
-      temp=queue->rear;
-      delete temp;
-      temp=queue->front;
-      int v=queue->count;
-      while((v-2)>0)
-      { temp=temp->link;
-        v--;}
-      cout<<"\nElement deleted";
-      queue->count--;
-      temp->link=NULL;
-      queue->rear=temp;
-      queue->front=queue->rear;}
-    else
-    { temp=new node;
-      temp=queue->rear;
-      delete temp;
-      temp=queue->front;
-      int v=queue->count;
-      while((v-2)>0)
-      { temp=temp->link;
-        v--;}
-      cout<<"\nElement deleted";
-      queue->count--;
-      temp->link=NULL;
-      queue->rear=temp;}}
-  
-  void destroy()
-  { if(queue->count==0)
-    { cout<<"\nList is empty";}
-    else
-    { temp2=new node;
-      temp=new node;
-      temp=queue->front;
-     while(queue->count<=0)
-     { temp2=temp;
-       temp=temp->link;
-       delete temp2;
-       queue->count--;}
-     delete temp;
-     cout<<"\nList destroyed";}}
-  
-  void empty()
-  { if(queue->front==NULL)
-    { cout<<"\nList is empty";}
-    else
-    { cout<<"\nList is not empty";}}
-  
-  void traversal()
-  { if(queue->count==0)
+    { cout<<"\nSLL: ";
+      while(temp->link!=NULL)
+      { cout<<temp->data;
+        cout<<"->";
+        temp=temp->link;}
+      cout<<temp->data;}}}
+  void deletenode()
+  { if(head->count==0)
     { cout<<"\nList is empty";}
     else{
-     int k,j=1;
+    ploc=new node;
+    pred=new node;
+    int i=0,g=0,d;
+    cout<<"\nEnter the data of the element to be deleted: ";
+    cin>>d;
+    ploc=head->first;
+    while(ploc->link!=NULL)
+    { if(ploc->data==d)
+      { g++;
+        if(i==0)
+        { head->first=ploc->link;
+          delete ploc;
+          head->count--;
+          cout<<"\nElement deleted";
+          break;}
+        else
+        { pred->link=ploc->link;
+          delete ploc;
+          head->count--;
+          cout<<"\nElement deleted";
+          break;}}
+        pred=ploc;
+        ploc=ploc->link;
+        i++;}
+       if(ploc->link==NULL && ploc->data==d)
+       { delete ploc;
+         cout<<"\nElement deleted";
+         pred->link==NULL;
+         head->count--;
+         g++;}
+       if(g==0)
+       { cout<<"\nElement not found in the list";}}}
+  void destroy()
+  { if(head->count==0)
+    { cout<<"\nList is empty";}
+    else{
+     ploc=new node;
+     temp=new node;
+     ploc=head->first;
+     while(ploc->link!=NULL)
+     { temp=ploc;
+       ploc=ploc->link;
+       delete temp;}
+     delete ploc;
+     cout<<"\nList destroyed";}}
+void empty()
+{ if(head->first==NULL)
+  { cout<<"\nList is empty";}
+  else
+  { cout<<"\nList is not empty";}}
+void traversal()
+{ if(head->count==0)
+  { cout<<"\nList is empty";}
+  else{
+   int k,j=1;
+   temp=new node;
+   pred=new node;
+   cout<<"\nEnter data of the element to be searched: ";
+   cin>>k;
+   temp=head->first;
+   pred->link=head->first;
+   while(temp->link!=NULL)
+   { if(temp->data==k)
+     { cout<<"\nElement id found in the list at "<<pred->link<<"and position "<<j;}
+     pred=temp;
+     temp=temp->link;
+     j++;}
+     if(temp->link==NULL)
+     { if(temp->data==k)
+       { cout<<"\nSearch element is found in the list at "<<pred->link<<" and position "<<j;}}}}
+void isfull()
+{ pnew=new node;
+  if(pnew==NULL)
+  {cout<<"\nList if full";}
+  else
+  {cout<<"\nList is not full";}}
+void retrieve()
+{ if(head->count==0)
+  { cout<<"\nList is empty";}
+  else{
+   int kq;
+   cout<<"\nEnter the target positon to be retrieved(list starts from 0): ";
+   cin>>kq;
+   if(kq>head->count)
+   {cout<<"\noverflow";}
+   else
+   { kq=kq-1;
      temp=new node;
      pred=new node;
-     cout<<"\nEnter data of the element to be searched: ";
-     cin>>k;
-     temp=queue->front;
-     pred->link=queue->front;
-     while(temp->link!=NULL)
-     { if(temp->data==k)
-       { cout<<"\nElement is found in the list at "<<pred->link<<"and position "<<j;}
-       pred=temp;
+     temp=head->first;
+     pred->link=head->first;
+     while(kq>=0)
+     { pred=temp;
        temp=temp->link;
-       j++;}
-       if(temp->link==NULL)
-       { if(temp->data==k)
-         { cout<<"\nSearch element is found in the list at "<<pred->link<<" and position "<<j;}}}}
-  
-  void isfull()
-  { pnew=new node;
-    if(pnew==NULL)
-    {cout<<"\nList if full";}
-    else
-    {cout<<"\nList is not full";}}
-
-  void retrieve()
-  { if(queue->count==0)
-    { cout<<"\nList is empty";}
-    else{
-     int kq;
-     cout<<"\nEnter the target position to be retrieved(starts from 0): ";
-     cin>>kq;
-     if(kq>queue->count)
-     {cout<<"\noverflow";}
-     else
-     { kq=kq-1;
-       temp=new node;
-       pred=new node;
-       temp=queue->front;
-       pred->link=queue->front;
-       while(kq>=0)
-       { pred=temp;
-         temp=temp->link;
-         kq--;}
-       cout<<"\nElement at target position is "<<temp->data<<" and address "<<pred->link;}}}
-
-  void stackcount()
-  { cout<<"\nStack count= "<<queue->count;}};
-
+       kq--;}
+     cout<<"\nElement at target position is "<<temp->data<<" and address "<<pred->link;}}}
+void listcount()
+{ cout<<"\nList count= "<<head->count;}};
 int main()
 { SLL s;
   int n,c;
   label:
-  cout<<"\n1.enqueue 2.dequeue 3.Display Queue 4.Destroy Queue 5.is empty 6.is full 7.Traversal 8.Retrieve 9. Queue count\nEnter choice: ";
+  cout<<"\n1.Insert 2.Delete 3.Display 4.Destroy 5.is empty 6.is full 7.Traversal 8.Retrieve 9.List count\nEnter choice: ";
   cin>>c;
   switch(c)
-  { case 1: s.enqueue();
+  { case 1: s.insert();
             break;
-    case 2: s.dequeue();
+    case 2: s.deletenode();
             break;
     case 3: s.display();
             break;
@@ -202,7 +185,7 @@ int main()
             break;
     case 8: s.retrieve();
             break;
-    case 9: s.stackcount();
+    case 9: s.listcount();
             break;}
  cout<<"\nPress 1 to continue and 0 to exit: ";
  cin>>n;
